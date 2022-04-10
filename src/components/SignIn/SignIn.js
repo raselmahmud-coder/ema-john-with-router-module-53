3e454347342dbac6ee50d5e639f03a88f07a65d8
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-04-10 17:04:16 +0800

    signin form added

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './SignIn.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css";
 const SignIn = () => {
-    return (
-        <div>
-            <h1>Sign In</h1>
+  return (
+    <div className="form-container">
+      <form>
+        <h1 className="form-title">Sign In</h1>
+        <div className="input-group">
+          <label htmlFor="email"> Email</label>
+          <input type="email" name="email" />
         </div>
-    );
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" />
+        </div>
+        <input className="form-submit" type="submit" value="Sign In" />
+      <p>
+        New to Ema-John?{" "}
+        <Link className="form-link" to={"/sign-up"}>
+          Create an Account{" "}
+        </Link>
+      </p>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
