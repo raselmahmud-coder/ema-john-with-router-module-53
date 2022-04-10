9bd573955d85cc2e27c2020e30a46d055d3f3331
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-04-10 22:50:53 +0800

    added login with authentication form

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,19 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase-init";
 import "./SignIn.css";
 const SignIn = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleUserSignIn = (e) => {
+      e.preventDefault();
+      signInWithEmailAndPassword(email, password)
+    };
+    const navigate = useNavigate();
+    if (user) {
+        navigate('/shop')
+    }
   return (
     <div className="form-container">
       <div>
         <h1 className="form-title">Sign In</h1>
-        <form>
+        <form onSubmit={handleUserSignIn}>
           <div className="input-group">
             <label htmlFor="email"> Email</label>
-            <input type="email" name="email" required />
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" required />
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
+            <p style={{ color: "red" }}>{error?.message}</p>
+            {loading ? <p style={{ color: "red" }}>Loading...</p> : ""}
           </div>
           <input className="form-submit" type="submit" value="Sign In" />
         </form>
