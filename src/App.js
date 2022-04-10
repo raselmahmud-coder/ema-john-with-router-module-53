e17790b24780ce0e50d167a51e309268fe950aaf
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-04-10 13:20:33 +0800

    sign in  and sign up router added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
+import SignIn from './components/SignIn/SignIn';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
       </Routes>
     </div>
   );
